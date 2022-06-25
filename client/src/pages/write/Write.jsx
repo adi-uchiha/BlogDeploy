a3175c9d879af64462122fb2aca8e53e18c1fac9
Adi12686 <adidacreator1@gmail.com>
2022-06-25 21:43:39 +0530

    Publish error msg and loading icon

--- client/src/pages/write/Write.jsx
+++ client/src/pages/write/Write.jsx
@@ -1,21 +1,42 @@
 import { useContext, useState } from "react";
 import "./write.css";
-import { Context } from "../../context/Context";
 import { axiosInstance } from "../../config";
+import { Context } from "../../context/Context";
 
 export default function Write() {
   const [title, setTitle] = useState("");
   const [desc, setDesc] = useState("");
   const [file, setFile] = useState(null);
   const { user } = useContext(Context);
-
+  const [imgMsg, setImgMsg] = useState(null);
+  const [publishBuffer, setPublishBuffer] = useState(false);
+  const [emptyData, setEmptyData] = useState(false);
   const handleSubmit = async (e) => {
+    
     e.preventDefault();
-    
-    
-    
+    console.log(title)
+    console.log(desc)    
+    if (title === ""){
+      setEmptyData(true)
+      return
+    }
+    if (desc === ''){
+      setEmptyData(true)
+      return
+    }
+    console.log("procced")
+    setEmptyData(false)
+    let postURL = ''
 
+    if(!file){
+      setImgMsg(true)
+      return
+    }
+
+    
     if (file) {
+      setImgMsg(false)
+      setPublishBuffer(true)
       const data = new FormData();
       const filename = Date.now() + file.name;
       data.append("name", filename);
@@ -27,11 +48,17 @@ export default function Write() {
           'content-type': 'multipart/form-data',
         }
       }
-      let postURL
-      try {
-        const imgres = await axiosInstance.post("/uploadPost", data, config);
-        postURL = imgres.data.url
-      } catch (err) {console.log(err)}
+
+
+      
+      if(file){
+
+        try {
+          const imgres = await axiosInstance.post("/uploadPost", data, config);
+          postURL = imgres.data.url
+        } catch (err) {console.log(err)}
+      }
+    
       const newPost = {
         username: user.username,
         title,
@@ -39,16 +66,23 @@ export default function Write() {
         postPhoto: postURL
       };
       try {
+
         const res = await axiosInstance.post("/posts", newPost);
         window.location.replace("/post/" + res.data._id);
       } catch (err) {}
+    
     }
+    setPublishBuffer(false)
   };
+
   return (
     <div className="write">
       {file && (
         <img className="writeImg" src={URL.createObjectURL(file)} alt="" />
       )}
+     
+      {imgMsg && <p>Give img!</p>}
+      {emptyData && <p>Title and body cannot be empty</p> }
       <form className="writeForm" onSubmit={handleSubmit}>
         <div className="writeFormGroup">
           <label htmlFor="fileInput">
@@ -58,7 +92,9 @@ export default function Write() {
             type="file"
             id="fileInput"
             style={{ display: "none" }}
-            onChange={(e) => setFile(e.target.files[0])}
+            onChange={(e) => {setFile(e.target.files[0])
+            setImgMsg(!imgMsg)
+            }}
           />
           <input
             type="text"
@@ -77,9 +113,11 @@ export default function Write() {
           ></textarea>
         </div>
         <button className="writeSubmit" type="submit">
-          Publish
+          {publishBuffer && <i class="fa fa-refresh fa-spin"></i>}Publish
         </button>
+
       </form>
+
     </div>
   );
 }
